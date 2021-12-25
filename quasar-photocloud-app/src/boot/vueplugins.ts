fc0252a76865fafed318f015f2e02a3d26830aad
Ludovic Lamarche <ludovic.lamarche@corp.ovh.com>
2021-12-25 09:29:50 +0100

    quasar vue plugin configuration

---
+++ quasar-photocloud-app/src/boot/vueplugins.ts
@@ -0,0 +1,8 @@
+// import Vue from 'vue'
+import { boot } from 'quasar/wrappers'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default boot(async ({ app }) => {
+    await Promise.resolve()
+    // vuePlugin.install(app)
+})
