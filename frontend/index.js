48771cc6612eafeb4deb8e9daaaa14976dd427ef
Ludovic Lamarche <snigle64@gmail.com>
2026-02-27 22:04:45 +0100

    fix expo build

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,8 @@
 import { registerRootComponent } from 'expo';
+import 'react-native-get-random-values';
+import { Buffer } from 'buffer';
+global.Buffer = Buffer; // Important pour certains paquets
+
 import App from './App';
 
 registerRootComponent(App);
